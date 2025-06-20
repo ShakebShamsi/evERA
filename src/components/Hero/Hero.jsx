02cd07e9925446303f0ac7ba7dbd4df6a5cfb8f2
ShakebShamsi <shakebshamsi@hotmail.com>
2025-06-20 20:44:31 +0530

    fix file name

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,4 +1,4 @@
-import './Hero.css'
+import './hero.css'
 import arrow_btn from '../../assets/arrow_btn.png'
 import play_icon from '../../assets/play_icon.png'
 import pause_icon from '../../assets/pause_icon.png'
